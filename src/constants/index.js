2026a3ed2e0e951f1841c8fe32c0879df098b239
Durgesh Salame <durgeshsalame2@gmail.com>
2025-02-03 22:39:42 +0530

    First Commit

---
+++ src/constants/index.js
@@ -0,0 +1,334 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    exp_logo,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    NETFLIX,
+    Feedback,
+    jobit,
+    tripguide,
+    threejs,
+    Portfolio,
+  } from "../assets";
+  
+   const navLinks = [
+    {
+      id: "hero",
+      title: "Home",
+    },
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "works",
+      title: "Works",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "Front-End Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Content Creator",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "Express JS",
+      icon: exp_logo,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    // {
+    //   name: "figma",
+    //   icon: figma,
+    // },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Full stack Developer",
+      company_name: "FRESERS",
+      icon: meta,
+      iconBg: "#E6DEDD",
+      date: "Aug 2023 - Present",
+      points: [
+        "My Name is Durgesh Salame I'm recently join Full Stack Devlopement (MERN) Class At. COSMOS DIGITAL from Wardha.",
+        "i have also undergone FRONTEND, BACKEND & DATABSE. ",
+        "In Frontend i'll learn HTML, CSS, JAVASCRIPT, BOOTSTRAP, MUI, AOS, Auth0, VANILLA.JS, Liabrary & Frameworks.",
+        "and Backend I'll learn Node.js, Express.js, MongoDB, Mongoose, SQL, & APIs "
+      ],
+    },
+    {
+      title: "Full stack Developer",
+      company_name: "FRESERS",
+      icon: meta,
+      iconBg: "#E6DEDD",
+      date: "Aug 2023 - Present",
+      points: [
+        "My Name is Durgesh Salame I'm recently join Full Stack Devlopement (MERN) Class At. COSMOS DIGITAL from Wardha.",
+        "i have also undergone FRONTEND, BACKEND & DATABSE. ",
+        "In Frontend i'll learn HTML, CSS, JAVASCRIPT, BOOTSTRAP, MUI, AOS, Auth0, VANILLA.JS, Liabrary & Frameworks.",
+        "and Backend I'll learn Node.js, Express.js, MongoDB, Mongoose, SQL, & APIs "
+      ],
+    },
+    {
+      title: "Full stack Developer",
+      company_name: "FRESERS",
+      icon: meta,
+      iconBg: "#E6DEDD",
+      date: "Aug 2023 - Present",
+      points: [
+        "My Name is Durgesh Salame I'm recently join Full Stack Devlopement (MERN) Class At. COSMOS DIGITAL from Wardha.",
+        "i have also undergone FRONTEND, BACKEND & DATABSE. ",
+        "In Frontend i'll learn HTML, CSS, JAVASCRIPT, BOOTSTRAP, MUI, AOS, Auth0, VANILLA.JS, Liabrary & Frameworks.",
+        "and Backend I'll learn Node.js, Express.js, MongoDB, Mongoose, SQL, & APIs "
+      ],
+    },
+    {
+      title: "Full stack Developer",
+      company_name: "FRESERS",
+      icon: meta,
+      iconBg: "#E6DEDD",
+      date: "Aug 2023 - Present",
+      points: [
+        "My Name is Durgesh Salame I'm recently join Full Stack Devlopement (MERN) Class At. COSMOS DIGITAL from Wardha.",
+        "i have also undergone FRONTEND, BACKEND & DATABSE. ",
+        "In Frontend i'll learn HTML, CSS, JAVASCRIPT, BOOTSTRAP, MUI, AOS, Auth0, VANILLA.JS, Liabrary & Frameworks.",
+        "and Backend I'll learn Node.js, Express.js, MongoDB, Mongoose, SQL, & APIs "
+      ],
+    },
+    // {
+    //   title: "React.js Developer",
+    //   company_name: "Starbucks",
+    //   icon: starbucks,
+    //   iconBg: "#383E56",
+    //   date: "March 2020 - April 2021",
+    //   points: [
+    //     "Developing and maintaining web applications using React.js and other related technologies.",
+    //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    //     "Implementing responsive design and ensuring cross-browser compatibility.",
+    //     "Participating in code reviews and providing constructive feedback to other developers.",
+    //   ],
+    // },
+    // {
+    //   title: "React Native Developer",
+    //   company_name: "Tesla",
+    //   icon: tesla,
+    //   iconBg: "#E6DEDD",
+    //   date: "Jan 2021 - Feb 2022",
+    //   points: [
+    //     "Developing and maintaining web applications using React.js and other related technologies.",
+    //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    //     "Implementing responsive design and ensuring cross-browser compatibility.",
+    //     "Participating in code reviews and providing constructive feedback to other developers.",
+    //   ],
+    // },
+    // {
+    //   title: "Web Developer",
+    //   company_name: "Shopify",
+    //   icon: shopify,
+    //   iconBg: "#383E56",
+    //   date: "Jan 2022 - Jan 2023",
+    //   points: [
+    //     "Developing and maintaining web applications using React.js and other related technologies.",
+    //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    //     "Implementing responsive design and ensuring cross-browser compatibility.",
+    //     "Participating in code reviews and providing constructive feedback to other developers.",
+    //   ],
+    // },
+    // {
+    //   title: "Full stack Developer",
+    //   company_name: "Meta",
+    //   icon: meta,
+    //   iconBg: "#E6DEDD",
+    //   date: "Jan 2023 - Present",
+    //   points: [
+    //     "Developing and maintaining web applications using React.js and other related technologies.",
+    //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    //     "Implementing responsive design and ensuring cross-browser compatibility.",
+    //     "Participating in code reviews and providing constructive feedback to other developers.",
+    //   ],
+    // },
+  ];
+  
+  const testimonials = [
+    {
+        // testimonial:
+        //   "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "This site under Construction",
+      // designation: "CFO",
+      // company: "Acme Co",
+      // image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    // {
+    //   testimonial:
+    //     "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    //   name: "Chris Brown",
+    //   designation: "COO",
+    //   company: "DEF Corp",
+    //   image: "https://randomuser.me/api/portraits/men/5.jpg",
+    // },
+    // {
+    //   testimonial:
+    //     "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    //   name: "Lisa Wang",
+    //   designation: "CTO",
+    //   company: "456 Enterprises",
+    //   image: "https://randomuser.me/api/portraits/women/6.jpg",
+    // },
+  ];
+  
+  const projects = [
+    {
+      name: "NETFLIX UI-Clone",
+      description:
+        "Welcome to the Netflix UI Landing Page Clone, meticulously crafted to replicate the inviting and streamlined design of the original Netflix homepage. Our clone offers users a familiar and seamless browsing experience, designed to captivate and engage from the moment they arrive. focusing on its visual appeal, functionality, and user engagement while emphasizing its role as a faithful reproduction for educational or developmental purposes.",
+      tags: [
+        {
+          name: "REACT",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "JSX",
+          color: "green-text-gradient",
+        },
+        {
+          name: "CSS",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: NETFLIX,
+      source_code_link: "https://github.com/DurgeshSalame",
+    },
+
+    {
+      name: "CUSTOMER FEEDBACK",
+      description:
+        "Introducing our Customer Feedback App, a powerful tool designed to empower businesses to listen, understand, and act upon customer feedback with precision and efficiency. Tailored for seamless integration into any customer service ecosystem, our app revolutionizes how businesses gather, analyze, and respond to customer insights, ensuring continuous improvement and unparalleled customer satisfaction.",
+      tags: [
+        {
+          name: "REACT",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "APIs",
+          color: "green-text-gradient",
+        },
+        {
+          name: "BOOTSTRAP",
+          color: "pink-text-gradient",
+        },
+        // {
+        //   name: "BACKEND",
+        //   color: "green-text-gradient",
+        // },
+      ],
+      image: Feedback,
+      source_code_link: "https://github.com/DurgeshSalame",
+    },
+    {
+      name: "My PORTFOLIO",
+      description:
+        "Welcome to the Web Developer Portfolio App, a dynamic platform designed specifically for web developers to showcase their skills, projects, and achievements with finesse and innovation. Tailored for professionals in front-end development, back-end development, full-stack development, or UI/UX design, our app is your gateway to impressing potential clients, employers, and collaborators in the digital world.",
+      tags: [
+        {
+          name: "REACT",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "THREE.JS",
+          color: "green-text-gradient",
+        },
+        {
+          name: "TAILWIND",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "VANILLA.JS",
+          color: "green-text-gradient",
+        },
+      ],
+      image: Portfolio,
+      source_code_link: "https://github.com/DurgeshSalame",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects, navLinks };
